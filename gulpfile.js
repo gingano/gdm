ea711c758a7b573932154f49e8ff35d7fb7c65b6
Oleksii Kovalenko <o.kovalenko@pozitiff.ua>
2020-08-10 20:29:40 +0300

    linter added

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,28 @@
-'use strict'
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const surge = require('gulp-surge')
+const ghPages = require('gulp-gh-pages')
 
-let gulp = require('gulp')
-let browserSync = require('browser-sync').create()
-let surge = require('gulp-surge')
-let ghPages = require('gulp-gh-pages')
-
-function fastDeploy () {
+function fastDeploy() {
   return surge({
     project: './dist',
-    domain: 'gdm.gingano.surge.sh'
+    domain: 'gdm.gingano.surge.sh',
   })
 }
 
 function watch() {
   browserSync.init({
     server: {
-      baseDir: './dist'
-    }
-  });
+      baseDir: './dist',
+    },
+  })
   gulp.watch('./css/*.css').on('change', browserSync.reload)
   gulp.watch('./dist/js/*js').on('change', browserSync.reload)
   gulp.watch('./dist/*.html').on('change', browserSync.reload)
 }
 
 function deploy() {
-  return gulp.src('./dist/**/*')
-    .pipe(ghPages());
+  return gulp.src('./dist/**/*').pipe(ghPages())
 }
 
 exports.watch = watch
